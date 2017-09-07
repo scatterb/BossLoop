f95dea61d7e461a5ea39efd70885e26918a267a2
Matsuuu <huhta.matias@gmail.com>
2017-09-07 18:04:18 +0300

    Menu set up

---
+++ BossLoop/Assets/_Scripts/SceneChanger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChanger : MonoBehaviour {
+
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Levelid: " + name);
+        Application.LoadLevel(name);
+    }
+
+}
