762d433400efb9b4d64e4fe50fb6bcb886f84f53
Jussi Ketomäki <jussi.ketomaki@edu.turkuamk.fi>
2017-09-26 21:14:11 +0300

    Player sprites turn according to button presses

--- BossLoop/Assets/_Scripts/PlayerScripts/PlayerController.cs
+++ BossLoop/Assets/_Scripts/PlayerScripts/PlayerController.cs
@@ -6,33 +6,39 @@ public class PlayerController : MonoBehaviour {
 
     public float speed = 10.0f;
     public Rigidbody2D rigidbody;
+    private Animator animator;
     Vector2 AxisInput;
 
     // Use this for initialization
     void Start () {
         rigidbody.freezeRotation = true;
+        animator = this.GetComponent<Animator>();
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        /*if(Input.GetKey(KeyCode.W))
+        if(Input.GetKey(KeyCode.W))
         {
-            transform.position += Vector3.up * speed * Time.deltaTime;
+            //transform.position += Vector3.up * speed * Time.deltaTime;
+            animator.SetInteger("Direction", 0);
         }
         if(Input.GetKey(KeyCode.S))
         {
-            transform.position += Vector3.down * speed * Time.deltaTime;
+            //transform.position += Vector3.down * speed * Time.deltaTime;
+            animator.SetInteger("Direction", 2);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position += Vector3.left * speed * Time.deltaTime;
+            //transform.position += Vector3.left * speed * Time.deltaTime;
+            animator.SetInteger("Direction", 3);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += Vector3.right * speed * Time.deltaTime;
+            //transform.position += Vector3.right * speed * Time.deltaTime;
+            animator.SetInteger("Direction", 1);
         }
-        */
+        
 
         Vector2 AxisInput = (new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
         
