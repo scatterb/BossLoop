efd9851b50e8e68e91438089e4502b1306b1660e
Matsuuu <huhta.matias@gmail.com>
2017-09-28 16:28:19 +0300

    Smoother animation transition

--- BossLoop/Assets/_Scripts/PlayerScripts/PlayerController.cs
+++ BossLoop/Assets/_Scripts/PlayerScripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour {
     private Animator animator;
     Vector2 AxisInput;
 
+
     // Use this for initialization
     void Start () {
         rigidbody.freezeRotation = true;
@@ -17,30 +18,34 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        
+        float verticalAxis = Input.GetAxis("Vertical");
+        float horizontalAxis = Input.GetAxis("Horizontal");
 
-        if(Input.GetKey(KeyCode.W))
+        if((verticalAxis > 0 && verticalAxis > horizontalAxis) || Input.GetKey(KeyCode.W))
         {
             //transform.position += Vector3.up * speed * Time.deltaTime;
             animator.SetInteger("Direction", 0);
         }
-        if(Input.GetKey(KeyCode.S))
+        if((verticalAxis < 0 && verticalAxis < horizontalAxis) || Input.GetKey(KeyCode.S))
         {
             //transform.position += Vector3.down * speed * Time.deltaTime;
             animator.SetInteger("Direction", 2);
         }
-        if (Input.GetKey(KeyCode.A))
+        if ((horizontalAxis < 0 && horizontalAxis < verticalAxis) || Input.GetKey(KeyCode.A))
         {
             //transform.position += Vector3.left * speed * Time.deltaTime;
             animator.SetInteger("Direction", 3);
         }
-        if (Input.GetKey(KeyCode.D))
+        if ((horizontalAxis > 0 && horizontalAxis > verticalAxis) || Input.GetKey(KeyCode.D))
         {
             //transform.position += Vector3.right * speed * Time.deltaTime;
             animator.SetInteger("Direction", 1);
         }
         
-
         Vector2 AxisInput = (new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+        
+
         
         rigidbody.velocity = AxisInput * speed;
 
